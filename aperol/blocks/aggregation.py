466c9dc20f96d1feab809e022ca028303b82d05d
Yuanqing Wang <wangyq+gh@wangyq.net>
2022-12-25 22:32:58 -0500

    attention aggregation

--- aperol/blocks/aggregation.py
+++ aperol/blocks/aggregation.py
@@ -9,11 +9,23 @@ class Aggregation(ParametrizedModule):
         self.linear_x = Linear()
         self.linear_y = Linear()
 
-    def forward(self, x, y):
-        """Aggregates y onto x and transform. """
+    def forward(self, x, y, config=None):
+        """Aggregates y onto x and transform.
+
+        Examples
+        --------
+        >>> x = torch.zeros(5, 3)
+        >>> y = torch.ones(4, 2)
+        >>> aggregation = Aggregation(torch.sum)
+        >>> z = aggregation(x, y)
+        >>> z.shape[0]
+        5
+        """
+        if config is None:
+            config = self.sample()
         y = self.aggregator(y, dim=0, keepdims=True)
-        y = self.linear_y(y)
-        x = self.linear_x(x)
+        y = self.linear_y(y, config=config)
+        x = self.linear_x(x, config=config)
         return x + y
 
 
@@ -27,10 +39,22 @@ class DotAttentionAggregation(ParametrizedModule):
         self.linear_q = Linear()
         self.linear_v = Linear()
 
-    def forward(self, x, y):
-        """Aggregates y onto x with dot product attention. """
-        k = self.linear_k(x)
-        q = self.linear_q(y)
-        a = (k @ q.t()).softmax(-1) # x, y
-        x = self.linear_v(a @ x) + x
+    def forward(self, x, y, config=None):
+        """Aggregates y onto x with dot product attention.
+
+        Examples
+        --------
+        >>> x = torch.zeros(5, 3)
+        >>> y = torch.ones(4, 2)
+        >>> aggregation = DotAttentionAggregation()
+        >>> z = aggregation(x, y)
+        >>> z.shape[0]
+        5
+        """
+        if config is None:
+            config = self.sample()
+        k = self.linear_k(x, config=config)
+        q = self.linear_q(y, config=config)
+        x = (k @ q.t()).softmax(-1) @ y # x, y
+        x = self.linear_v(x)
         return x
