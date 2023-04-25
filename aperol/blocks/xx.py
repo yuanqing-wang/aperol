ab9fa9d8a3988de09bcdac733188c750eeb73b41
Yuanqing Wang <wangyq+gh@wangyq.net>
2023-04-25 16:36:38 +0800

    geometry operation

--- aperol/blocks/xx.py
+++ aperol/blocks/xx.py
@@ -20,20 +20,18 @@ class GeometryUpdate(Block):
         >>> v = torch.zeros(2, 5)
         >>> e = torch.zeros(2, 2, 4)
         >>> x = torch.zeros(2, 3, 6)
+        >>> p = torch.zeros(2, 3, 7)
         >>> config = geometry_update.Config(10)
-        >>> v1, e1, x1 = geometry_update(v, e, x, config=config)
-        >>> list(x1.shape)
-        [2, 3, 11]
+        >>> v1, e1, x1, p1 = geometry_update(v, e, x, p, config=config)
+        >>> list(p.shape)
+        [2, 3, 10]
         >>> assert torch.isclose(v1, v).all()
         >>> assert torch.isclose(e1, e).all()
         """
         if config is None:
             config = self.sample()
-        x0 = x[..., :1]
-        x1 = x[..., 1:]
-        x1 = self.linear(x1, config=config)
-        x = torch.cat([x0, x1], dim=-1)
-        return v, e, x
+        p = self.linear(p, config=config)
+        return v, e, x, p
 
 class GeometryReduce(Block):
     def __init__(self):
@@ -42,7 +40,7 @@ class GeometryReduce(Block):
             activation=None, bias=False, max_in=MAX_IN-1, max_out=1, min_out=0,
         )
 
-    def forward(self, v, e, x, config=None):
+    def forward(self, v, e, x, p, config=None):
         """
         Examples
         --------
@@ -50,16 +48,14 @@ class GeometryReduce(Block):
         >>> v = torch.zeros(2, 5)
         >>> e = torch.zeros(2, 2, 4)
         >>> x = torch.zeros(2, 3, 6)
-        >>> v1, e1, x1 = geometry_reduce(v, e, x)
+        >>> p = torch.zeros(2, 3, 7)
+        >>> v1, e1, x1, p1 = geometry_reduce(v, e, x, p)
         >>> list(x1.shape)
         [2, 3, 6]
         >>> assert torch.isclose(v1, v).all()
         >>> assert torch.isclose(e1, e).all()
         """
         config = self.linear.Config(1)
-        x0 = x[..., :1]
-        x1 = x[..., 1:]
-        delta_x0 = self.linear(x1, config=config)
-        x0 = x0 + delta_x0
-        x = torch.cat([x0, x1], dim=-1)
-        return v, e, x
+        delta_x = self.linear(p, config=config)
+        x = x + delta_x
+        return v, e, x, p
